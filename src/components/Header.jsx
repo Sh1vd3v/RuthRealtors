8d70ed396e3d8d3ce17f522983b23fab41b6faf9
KARTOOS98 <shivam.dev9@gmail.com>
2025-09-01 23:13:31 +0530

    day 1

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export const Header = () => (
+  <>
+    <div className="flex items-center justify-center h-20">
+      <h1 className="text-amber-500 text-6xl font-bold font-imperial-script">
+        Ruth Realtors
+      </h1>
+    </div>
+  </>
+);
