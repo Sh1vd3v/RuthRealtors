41586bcc01f3b444bef47e50e097475f2be4abd2
KARTOOS98 <shivam.dev9@gmail.com>
2025-09-02 23:45:05 +0530

    day 2(added motion + logo)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,29 @@
+import rrImage from "../assets/rr-logo.png";
+import { motion } from "framer-motion";
+
 export const Header = () => (
   <>
-    <div className="flex items-center justify-center h-20">
-      <h1 className="text-amber-500 text-6xl font-bold font-imperial-script">
+    <div className="grid grid-cols-3 items-center h-20 px-4">
+      <motion.div
+        initial={{ x: -100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 0.5, ease: "easeOut" }}
+        className="flex justify-start"
+      >
+        <img
+          src={rrImage}
+          alt="rr_logo"
+          className="flex justify-start w-12 h-12 sm:w-16 sm:h-16"
+        />
+      </motion.div>
+      <motion.h1
+        initial={{ y: -50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, ease: "easeOut" }}
+        className="text-amber-500 text-2xl sm:text-4xl font-bold font-imperial-script text-center"
+      >
         Ruth Realtors
-      </h1>
+      </motion.h1>
     </div>
   </>
 );
